e4c907489a9a4fbe6f088f9912128b4e54a0bb2b
Sandip Sen <umel@protonmail.com>
2017-10-09 21:56:50 +0530

    First commit completes the 3 X 3 board

---
+++ src/minesweeper.js
@@ -0,0 +1,20 @@
+//This constant variable holds the pipe character used to create the grid
+const blankLine = '  |   |  ';
+
+console.log('This is what an empty board would look like:');
+
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+//This constant variable holds the guess(bomb) in the first square
+const guessLine = '1 |   |  ';
+
+//This constant variable holds the revealed bomb
+const bombLine = '  | B |  ';
+
+console.log('This is what a board with a guess and a bomb on it would look like:');
+
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
