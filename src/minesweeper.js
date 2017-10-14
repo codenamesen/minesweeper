08c9b990f787790cf6e86f79045c48316984067c
Sandip Sen <umel@protonmail.com>
2017-10-14 21:20:14 +0530

    Add player Interactivity Code

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -47,15 +47,65 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     let randomRowIndex = Math.floor(Math.random() * numberOfRows);
     //Generate random column index
     let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-    //Place a Bomb at the random row and column
-    board[randomRowIndex][randomColumnIndex] = 'B';
-    //Increment the counter
-    numberOfBombsPlaced++;
+    //Checks if the square already has a bomb
+    if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+      //Place a Bomb at the random row and column
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      //Increment the counter
+      numberOfBombsPlaced++;
+    }
   }
   //return the finally generated board
   //This has the flaw of placeing bomb in place already consisting bomb. This needs to be fixed.
   return board;
 };
+
+//This constant holds the function that calculates the no. of adjacent bombs to a squeare
+const getNumberOfNeighbourBombs = (bombBoard, rowIndex, columnIndex) => {
+  //This constant stores the array which represents the neighbouring 8 squares
+  const neighborOffsets = [[-1, -1], [-1,0], [-1, 1], [0, -1], [0, 0], [0, 1], [1, 0], [1, 1]];
+  //This constant stores the total no. of rows present in the board
+  const numberOfRows = bombBoard.length;
+  //This constant stores the total no. of column present in the board
+  const numberOfColumns = bombBoard[0].length;
+  //This constatnt will be used to store the no. of bombs present to the adjacent of the squeare clicked
+  let numberOfBombs = 0;
+
+  //This iterator will check all the neighboring squares for the presence of bombs
+  neighborOffsets.forEach(offset => {
+    //Add the row
+    const neighborRowIndex = rowIndex + offset[0];
+    const neighborColumnIndex = columnIndex + offset[1];
+    //Checks if the adjacent squeares do exists
+    if (neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns) {
+      //Checks if bomb is present in the selected adjacent square
+      if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+        //If bomb is present the counter is incremented by one.
+        numberOfBombs++;
+      }
+    }
+  });
+
+  //Returns totoal no. of Bomb present in the adjacent squares
+  return numberOfBombs;
+};
+
+//This constant stores the function that allows the user to filp tile.
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
+  //Checks if the flipped tile is empty
+  if (playerBoard[rowIndex][columnIndex] !== ' ') {
+    console.log('This tile has already been flipped');
+    return;
+    //Checks if the flipped tile has a bomb in the corresponding square in bombBoard
+  } else if (bombBoard[rowIndex][columnIndex] === 'B') {
+    //If there is a bomb in the boamboard mark it with 'B' in the playerBoard
+    playerBoard[rowIndex][columnIndex] = 'B';
+  } else {
+    //Puts the total no of bombs present in the neighouring squares
+    playerBoard[rowIndex][columnIndex] = getNumberOfNeighbourBombs(bombBoard, rowIndex, columnIndex);
+  }
+};
+
 
 //This constant will hold the function that will print the board.
 const printBoard = board => console.log(board.map(row => row.join('|')).join('\n'));
@@ -71,3 +121,11 @@ printBoard(playerBoard);
 //Prints the Bomb board
 console.log('Bomb Board: ');
 printBoard(bombBoard);
+
+//call the fliptile function with the board and the idexes for flipping the tile.
+flipTile(playerBoard, bombBoard, 0, 0);
+
+console.log('Updated Player Board: ')
+
+//Prints the updated Player board
+printBoard(playerBoard);
